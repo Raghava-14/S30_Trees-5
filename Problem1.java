1a740dd24a2a0d35afe8e0d40af4ada88c11a488
Raghava-14 <73310160+Raghava-14@users.noreply.github.com>
2024-08-21 18:59:40 -0700

    Completed Trees-5

--- Problem1.java
+++ Problem1.java
@@ -1,28 +1,6 @@
 //Time = O(n), n is numbe of nodes in a binary tree
 //Space = O(w), w is max width of binary tree
 
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
 
 class Solution {
     public Node connect(Node root) {
--- Problem2.java
+++ Problem2.java
@@ -1,21 +1,6 @@
 //Time = O(n), number of nodes
 //Space = O(h), height of binary tree
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     // Initialize the prev, first, and second nodes as null
     private TreeNode prev, first, second;
